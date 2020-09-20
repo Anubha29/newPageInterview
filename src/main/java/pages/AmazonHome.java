db2135124c4d43cf0d503762cb935007cf7b67ae
Anubha <anumathur29@gmail.com>
2020-09-20 18:43:39 +0530

    get attribute to get text

--- src/main/java/pages/AmazonHome.java
+++ src/main/java/pages/AmazonHome.java
@@ -8,7 +8,7 @@ public class AmazonHome extends Base implements ShoppingSite{
 
     public String getItemPrice(WebDriver driver) {
         String price="";
-        price=driver.findElement(itemPrice).getAttribute("value");
-        return price;
+        price=driver.findElement(itemPrice).getText();
+        return price.replace("₹","");
     }
 }
