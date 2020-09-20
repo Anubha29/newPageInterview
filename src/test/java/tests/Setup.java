ff6d55cd548972de0477429e539ae0eb79b002cb
Anubha <anumathur29@gmail.com>
2020-09-20 18:42:37 +0530

    Changes missed
    Changes were not saved due to sudden powercut

--- src/test/java/tests/Setup.java
+++ src/test/java/tests/Setup.java
@@ -3,7 +3,19 @@ package tests;
 import pages.Base;
 
 public class Setup extends Base {
+    @BeforeSuite
     public void init(){
+        System.out.println("Inside init");
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver_win32/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://www.mysmartprice.com/mobile/apple-iphone-7-msp10208");
+        System.out.println("Exiting init");
+    }
+
+    @AfterSuite
+    public void cleanup(){
+        driver.quit();
     }
 }
